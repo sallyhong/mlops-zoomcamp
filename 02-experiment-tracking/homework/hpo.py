3ce20fd4ca0553f2af90e79f43fa3aedaf848911
Sally Hong <sally.hong@gmail.com>
2024-05-27 14:46:42 -0500

    Pushing HW 2

--- 02-experiment-tracking/homework/hpo.py
+++ 02-experiment-tracking/homework/hpo.py
@@ -34,13 +34,17 @@ def run_optimization(data_path: str, num_trials: int):
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
     def objective(params):
-
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
-
-        return {'loss': rmse, 'status': STATUS_OK}
+        with mlflow.start_run():
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = mean_squared_error(y_val, y_pred, squared=False)
+    
+            # Log parameters and RMSE to MLflow
+            mlflow.log_metric("rmse", rmse)
+            mlflow.log_params(params)
+    
+            return {'loss': rmse, 'status': STATUS_OK}
 
     search_space = {
         'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
--- 02-experiment-tracking/homework/register_model.py
+++ 02-experiment-tracking/homework/register_model.py
@@ -70,10 +70,18 @@ def run_register_model(data_path: str, top_n: int):
 
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=1,
+        order_by=["metrics.test_rmse ASC"]
+    )[0]
 
     # Register the best model
-    # mlflow.register_model( ... )
+    model_uri = f"runs:/{best_run.info.run_id}/model"
+    model_name = "RandomForestRegressorBestModel"
+    mlflow.register_model(model_uri=model_uri, name=model_name)
+    print(f"Registered model {model_name} with run ID {best_run.info.run_id}")
 
 
 if __name__ == '__main__':
